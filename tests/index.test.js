8b86795ca9e645d9312833510fddfd127f40f74f
Jacques <jacques.bonet.e@thalesdigital.io>
2021-01-01 02:36:09 +0100

    remove uneccessary file

--- tests/index.test.js
+++ tests/index.test.js
@@ -5,23 +5,23 @@ const testDist = process.env.LIB_DIR === 'dist';
 describe('antd dist files', () => {
   it('exports modules correctly', () => {
     // eslint-disable-next-line global-require,import/no-unresolved
-    const antd = testDist ? require('../dist/antd') : require('../components');
+    const antd = testDist ? require('../dist/ant-design-layout') : require('../components');
     expect(Object.keys(antd)).toMatchSnapshot();
   });
 
   // https://github.com/ant-design/ant-design/issues/1970
   // https://github.com/ant-design/ant-design/issues/1804
   if (testDist) {
-    it('antd.js should export version', () => {
+    it('ant design layout should export version', () => {
       // eslint-disable-next-line global-require,import/no-unresolved
-      const antd = require('../dist/antd');
+      const antd = require('../dist/ant-design-layout');
       expect(antd).toBeTruthy();
       expect(antd.version).toBe(pkg.version);
     });
 
-    it('antd.min.js should export version', () => {
+    it('ant-design-layout should export version', () => {
       // eslint-disable-next-line global-require,import/no-unresolved
-      const antd = require('../dist/antd.min');
+      const antd = require('../dist/ant-design-layout.min');
       expect(antd).toBeTruthy();
       expect(antd.version).toBe(pkg.version);
     });
